d846350d8f9104c23b29ab6ed2b3306eabab11cf
bernardogarza <bernardo.g.landa@gmail.com>
2019-12-19 12:03:04 -0600

    installed node-sass and create style files for the components

--- src/components/app/App.component.jsx
+++ src/components/app/App.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Display from '../display/display.component';
 import ButtonPanel from '../button-panel/button-panel.component';
 
+import './App.style.scss'
+
 const App = () => (
   <div>
     <Display />
