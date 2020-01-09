a25d10beb743c550c2791e78afcf7105258daa35
bernardogarza <bernardo.g.landa@gmail.com>
2020-01-09 15:12:57 -0600

    Fixed the divison problem

--- src/logic/calculate.jsx
+++ src/logic/calculate.jsx
@@ -2,7 +2,7 @@ import operate from './operate';
 
 const Calculate = (obj, buttonName) => {
   const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'].includes(buttonName);
-  const operations = ['+', '-', 'X', '%', '+/-'].includes(buttonName);
+  const operations = ['+', '-', 'X', '%', '+/-', '÷'].includes(buttonName);
   const equals = ['='].includes(buttonName);
   let { total, next, operation } = obj;
 
