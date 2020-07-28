dee1e5c97f5d93b12bb5d47fb3fd81c0ef472e49
Bernardo Garza <bernardo.g.landa@gmail.com>
2020-07-28 10:14:56 -0500

    Updates

---
+++ precache-manifest.a80d7209b217dc84311e109a11eac6ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f368231ada229cdf83d99273c640e288",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c286a7d892a4033e25cb",
+    "url": "/static/css/main.c59a2670.chunk.css"
+  },
+  {
+    "revision": "d598f4aca49952eff1cb",
+    "url": "/static/js/2.67a20b47.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.67a20b47.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c286a7d892a4033e25cb",
+    "url": "/static/js/main.467b6501.chunk.js"
+  },
+  {
+    "revision": "e7cd3c10a4d163ef884c",
+    "url": "/static/js/runtime-main.4cd2a8eb.js"
+  }
+]);
